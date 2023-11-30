75d6ed659c8b278429f8b0d2314f49fcf9575050
Ahmed Mohamed <am9718366@gmail.com>
2023-11-30 23:43:40 +0200

    l

--- reactpro/src/index.js
+++ reactpro/src/index.js
@@ -10,7 +10,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<div className='g'>
   <BrowserRouter>
    <UserProvider>
-  <App />
+  <App /> 
    </UserProvider>
   </BrowserRouter>
   
